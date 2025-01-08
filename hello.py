cb6d0886cdb5048d8f8523be22c7656dc6732574
HanadiAlsalman <alsalmanhanadi@gmail.com>
2025-01-08 14:23:04 +0100

    new file

---
+++ hello.py
@@ -0,0 +1,90 @@
+# hello.world.py
+print("beräkna summan a = 10 och y = 20")
+a = 10
+y = 20
+print ("summan =", a + y)
+
+#namn = "Hanadi"
+#myAge = 38
+#print("my name is", namn, " my age is ", myAge)
+
+#personnummer = 123456778888
+#vikt = 60.56
+#namn = "Hanadi"
+#ärDuKvinna = True
+#print ( namn, "är du kvinna?", ärDuKvinna, " vikt", vikt, " personnummer", personnummer)
+
+
+#print (" beräkna multiplicera x = 123 och y = 2")
+#x = "123"
+#y = 2
+#print("summa är ", int(x) * y)
+#print("summa är ", x * y)
+
+#float_tal = 0.1 + 0.2
+#print (float_tal)
+
+#decimal_tal = float(0.1 + 0.2)
+#print (decimal_tal)
+
+#pi = 3.14
+#heltal_pi = int(pi)
+#print(heltal_pi)
+#x = 5
+#floattal_x = float(x)
+#print(floattal_x)
+
+#x = input("skriva första talet:")
+#y = input("skriva andra talet:")
+#summa = int(x) + int(y)
+#print (f"summan är : {summa}")
+
+#text = "Data Science"
+
+#print(text[0])
+
+#print(text[-1])
+
+#no_witespace = text.strip()
+
+#print (no_witespace)
+
+#print ("Hej")
+#namn = input ("vad heter du?")
+#ålder = input ("hur gammal är du")
+#print ("Hej", namn, "Du är", int(ålder), "år gammal" )
+
+
+#finally:
+  # print("Programmet fortsätter.")
+
+#x = 10 / 0
+# Bör ge syntaxfel
+# x = 10 / 0
+
+# Kommer inte att nå hit ner
+#print("Men jag vill ju fortsätta")
+#print("Nu är vi klara.")
+#"""
+#Work in progress
+#def delaMedFunction(tal):
+   # try:
+        #print(f"10 / {tal}")
+       # x = 10 / tal
+       # print(f"10 / {tal} = {x}")
+   # except ZeroDivisionError:
+      #  print("Du kan inte dela med 0.")
+        # Kör någon annan kod här
+        # Hantera felet
+     #   return  False
+   # except:
+     #   print("Vi fick ett fel.")
+    #finally:
+      #  print("Försök igen.")
+      #  z = input("Skriv in ett tal att dela med: ")
+       # delaMedFunction(z)
+#q = input("Skriv in ett tal att dela med: ")
+#delaMedFunction(q)
+#"""
+
+
